b8616db2371aa4d73cb01808c12856fd8995cabf
Tarso Galvão <54255356+surtarso@users.noreply.github.com>
2024-09-09 23:04:45 -0300

    added favicon placeholder
    generic favicon in base64

--- index.php
+++ index.php
@@ -19,6 +19,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <!-- favicon placeholder -->
+    <link rel="shortcut icon" type="image/x-icon" href="data:image/x-icon;base64,c2t1bGw">
     <!-- Dynamic page title with capitalized folder name -->
     <title><?php echo isset($headerTitle) && !empty($headerTitle) ? $headerTitle : $defaultTitle; ?></title>
     <style>
